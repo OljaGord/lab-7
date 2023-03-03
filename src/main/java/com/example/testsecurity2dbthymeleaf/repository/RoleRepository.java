f98db2c42eb655cf52588646b48f2e9415f43172
Gordeeva Olga <olja.gord00@gmail.com>
2023-03-03 17:07:49 +0500

    lab-7

---
+++ src/main/java/com/example/testsecurity2dbthymeleaf/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.example.testsecurity2dbthymeleaf.repository;
+
+import com.example.testsecurity2dbthymeleaf.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Role findByName(String name);
+}
---
+++ src/main/java/com/example/testsecurity2dbthymeleaf/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.testsecurity2dbthymeleaf.repository;
+
+import com.example.testsecurity2dbthymeleaf.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByEmail(String email);
+}
